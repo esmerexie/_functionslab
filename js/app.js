6cb115e37fb59ecd9deee5c6c41ef27fe88573ef
Rexie Esmeralda <esmeraldarexie@gmail.com>
2022-06-13 00:08:42 -0700

    passed multiply and sum test

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
   let number = a * b;
-  return[number, 'The product of ' + a + ' and ' + b + ' is ' + number + '.']
+  return[number, 'The product of ' + a + ' and ' + b + ' is ' + number + '.'];
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -52,11 +52,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let fSum = sum(a,b)[0];
+  let tSum = sum(c, fSum)[0];
+  let fMultiply = multiply(a,b)[0];
+  let tMultiply = multiply(c, fMultiply)[0];
 
+  return[tSum, tMultiply, `${a} and ${b} and ${c} sum to ${tSum}.` , `The product of ${a} and ${b} and ${c} is ${tMultiply}.`];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
